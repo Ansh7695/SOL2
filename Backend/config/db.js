a600262952d3bd3722647a385c35d04a17c43193
Ansh <anshsharma0824@gmail.com>
2026-02-03 13:30:52 +0530

    Fix: Correct MongoDB connection logic for Atlas URIs

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -4,7 +4,8 @@ const connectDB = async () => {
     mongoose.connection.on('connected', () => {
         console.log("DB Connection Established");
     });
-    await mongoose.connect(`${process.env.MONGODB_URI}/schoolofnature`);
+    // Use dbName option to safely select the database, regardingless of query params
+    await mongoose.connect(process.env.MONGODB_URI, { dbName: 'schoolofnature' });
 }
 
 export default connectDB;
